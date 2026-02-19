57ddd6cb0a398318e2e284d413f09e4740d5f8b8
kelley <kelleycastillo@outlook.com>
2026-02-19 15:06:17 -0800

    added a form for users to sign up

---
+++ views/create_userform.js
@@ -0,0 +1,30 @@
+
+/** form for signing up  */
+document.getElementById("app-form").onsubmit = () => {
+
+    clearErrors();
+
+    let isValid = true;
+    
+    //Validate first name
+    let fname = document.getElementById("fname").value.trim();
+    if(!fname) {
+        document.getElementById("err-fname").style.display = "block";
+        isValid = false;
+    }
+
+    //Validate last name
+    let lname = document.getElementById("lname").value.trim();
+    if(!lname) {
+        document.getElementById("err-lname").style.display = "block";
+        isValid = false;
+    }
+    return isValid;
+}
+
+function clearErrors() {
+    let errors = document.getElementsByClassName("err");
+    for (let i=0; i<errors.length; i++) {
+        errors[i].style.display = "none";
+    }
+}
