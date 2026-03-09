d909cd687c5c77073225faf8c90ad4353a72ce0d
AaronB22 <aaronthesnowboarder@live.com>
2026-03-09 11:47:19 -0700

    added db setup and signup route

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 import express from "express";
+import dotenv from 'dotenv';
+import mysql2 from 'mysql2';
+dotenv.config();
 
 const app = express();
 const PORT = 3003;
 const user = { name: "" };
 const submissions = [];
+const pool = mysql2.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+}).promise();
 
 app.set("view engine", 'ejs');
 
 app.use(express.static('public'));
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
@@ -22,22 +33,28 @@ app.get('/admin', (req, res) => {
     res.render('admin', { user, submissions });
 });
 
-app.post('/signup', (req, res) => {
-    const { fname, lname, email } = req.body;
+app.post('/signup', async(req, res) => {
 
-    const record = {
-        fname,
-        lname,
-        email,
-        timestamp: new Date().toLocaleString()
-    };
 
-    submissions.push(record);
+    const user = req.body;
+    const params=[
+        user.fname,
+        user.lname,
+        user.email,
+        user.password
+    ]
+    console.log(params)
+    const sql = `INSERT INTO users(fname,lname, email,password)
+                  values (?,?,?,?);`;
+
+    const result = await pool.execute(sql, params);
+    const [count]= await pool.query(`Select COUNT(*) As count FROM users`)
+
 
     res.render('confirmation_userform', {
         user,
-        formData: record,
-        submissionCount: submissions.length
+        formData: result,
+        submissionCount: count[0].count
     });
 });
 app.post('/upload', (req, res) => {
