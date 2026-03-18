0472c4cbb38a8e75d0b9dc6dcecc961901a82c06
AaronB22 <aaronthesnowboarder@live.com>
2026-03-18 00:16:37 -0700

    fixed user info being passed in

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ import dotenv from 'dotenv';
 import mysql2 from 'mysql2';
 import multer from 'multer';
 import path from 'path';
+import session from "express-session";
 import { validate_upload, validate_signup } from "./validate.js";
 dotenv.config();
 
 
 const app = express();
 const PORT = 3003;
-const user = { name: "" };
 const pool = mysql2.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -47,6 +47,12 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+    secret:'key',
+    resave:false,
+    saveUninitialized:true
+}))
+
 // Main route renders the home page
 app.get('/', async (req, res) => {
     try {
@@ -54,23 +60,23 @@ app.get('/', async (req, res) => {
         res.render('home', { user, cards });
     } catch (err) {
         console.error(err);
-        res.render('home', { user, cards: [] });
+        res.render('home', { user: req.session.user, cards: [] });
     }
 });
 
 // Sign-up page route
 app.get('/signup', (req, res) => {
-    res.render('signup', { user, errors:null });
+    res.render('signup', { user: req.session.user, errors:null });
 });
 
 // Profile page route
 app.get('/profile', (req, res) => {
-    res.render('profile', { user });
+    res.render('profile', { user: req.session.user });
 });
 
 // Sign-in page route
 app.get('/signin', (req, res) => {
-    res.render('signin', { user, error: false });
+    res.render('signin', { user: req.session.user, error: false });
 });
 
 // Sign-in submission route
@@ -86,9 +92,16 @@ app.post('/signinsubmit', async (req, res) => {
             [req.body.email]
         );
         if (users[0].password === req.body.password) {
-            res.redirect('/');
+            const returnUser={
+                "fname":users[0].fname,
+                "lname":users[0].lname,
+                "email":users[0].email
+            }
+            req.session.user=returnUser;
+            console.log(req.session.user)
+            res.render('profile',{ user: req.session.user });
         } else {
-            res.render('signin', { user, error: true });
+            res.render('signin', { user:req.session.user, error: true });
         }
     } catch (err) {
         res.status(500).send('Error loading orders' + err.message);
@@ -117,7 +130,7 @@ app.post('/signup', async (req, res) => {
     ];
     const valid=validate_signup(req.body)
     if(!valid.isValid){
-        res.render('signup',{errors:valid.errors})
+        res.render('signup', { user: req.session.user, errors:valid.errors})
         return;
     }
     const sql = `INSERT INTO users(fname, lname, email, password) VALUES (?, ?, ?, ?)`;
@@ -137,7 +150,7 @@ app.post('/upload', upload.single('imgUpload'), async (req, res) => {
     //validates upload page
     const valid = validate_upload(req.body)
     if (!valid.isValid) {
-        res.render('upload', { errors: valid.errors })
+        res.render('upload', { user: req.session.user,errors: valid.errors })
         return;
     }
     // If a file was uploaded use its filename, otherwise store null
@@ -162,7 +175,7 @@ app.get('/cards', async (req, res) => {
     try {
         const category = req.query.category || 'all';
         const [submissions] = await pool.query('SELECT * FROM cards ORDER BY timestamp DESC');
-        res.render('cards', { user, submissions, activeCategory: category });
+        res.render('cards', { user: req.session.user, submissions, activeCategory: category });
     } catch (err) {
         console.error('Database error:', err);
         res.status(500).send('Error loading cards: ' + err.message);
@@ -171,7 +184,7 @@ app.get('/cards', async (req, res) => {
 
 // Upload page route
 app.get('/upload', (req, res) => {
-    res.render('upload', { user, errors: null });
+    res.render('upload', { user: req.session.user, errors: null });
 });
 
 // Start server
